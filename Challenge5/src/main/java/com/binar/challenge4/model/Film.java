59ef7d69b5210b96cd9c15631fcb7f1eaded024c
DwiputraAditya <you@example.com>
2023-04-28 20:47:42 +0700

    Commit Base Project

---
+++ Challenge5/src/main/java/com/binar/challenge4/model/Film.java
@@ -0,0 +1,23 @@
+package com.binar.challenge4.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "film")
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long filmCode;
+    private String filmName;
+    private Boolean isPremiered;
+
+    @OneToMany(mappedBy = "film")
+    @JsonIgnore
+    private List<Schedule> schedule = new ArrayList<>();
+}
---
+++ Challenge5/src/main/java/com/binar/challenge4/model/User.java
@@ -0,0 +1,17 @@
+package com.binar.challenge4.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String userName;
+    private String email;
+    private String password;
+}
